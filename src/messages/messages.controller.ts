b3d12ebe2099bf072a87c3e345f8f7abfde76d54
jhonnatan suarez <jonathan.sm87@gmail.com>
2022-06-15 10:48:12 -0500

    terminado

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.services';
+
+@Controller('messages')
+export class MessagesController {
+
+    constructor(public messagesService: MessagesService) {}
+
+    @Get()
+    listMessages() {
+        return this.messagesService.findAll();
+    }
+    
+    @Post()
+    createMessage(@Body() body: CreateMessageDto) {
+        return this.messagesService.create(body.content);
+    }
+    
+    @Get('/:id')
+    async getMessage(@Param('id') id: string) {
+        const message = await this.messagesService.findOne(id);
+
+        if (!message) {
+            throw new NotFoundException('message not found');
+        }
+
+        return message
+    }
+}
+
+// @Post()
+// createMessage(@Body() body: any) {
+//     console.log(body);
+// }
